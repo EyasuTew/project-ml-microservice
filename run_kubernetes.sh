ea4b8e6f13049d325f06317327f0abbabe47c017
Eyasu Tewodros <eyasutew1@gmail.com>
2022-07-29 20:19:04 +0300

    update

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,30 +5,6 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-dockerpath=eyasutew/ml-microservice:v.01
-
-# Step 2
-# Run the Docker Hub container with kubernetes
-kubectl create deployment project-ml-microservice-first --image=$dockerpath
-
-# Step 3:
-# List kubernetes pods
-kubectl get pods --all-namespaces
-
-# Step 4:
-# Forward the container port to a host
-kubectl port-forward deployment/project-ml-microservice-first 8000:80
-
-
-
-
-#!/usr/bin/env bash
-
-# This tags and uploads an image to Docker Hub
-
-# Step 1:
-# This is your Docker ID/path
-# dockerpath=<>
 dockerpath="eyasutew/ml-microservice"
 
 # Step 2
@@ -44,6 +20,6 @@ kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward mlmicroserviceapi 8000:80
+kubectl port-forward mlmicroservice 8000:80
 #log container outputs
 kubectl log --follow mlmicroservice
